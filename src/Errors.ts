f8c4810f27840892eb0690f9e25ef5dd35b7d275
Clément Boittin <clement.boittin@protonmail.com>
2022-03-18 22:03:13 +0100

    ✏️ Fix typo

--- src/Errors.ts
+++ src/Errors.ts
@@ -2,7 +2,7 @@ import { serviceName } from ".";
 
 export class WrongCredentialsError extends Error {
   constructor(serviceName: serviceName) {
-    super(`Wrong credentials error raised when login to ${serviceName}`);
+    super(`Wrong credentials error raised while login to ${serviceName}`);
     this.name = "WrongCredentialsError";
   }
 }
